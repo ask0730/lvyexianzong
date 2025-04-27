ffbc215615e322df64832ce28ff1d95b584f324e
ask <2137576215@qq.com>
2025-04-27 10:19:24 +0800

    初始化管理员账户

--- web-server/config/db.config.js
+++ web-server/config/db.config.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/company-system')
-  .then(() => {
+  .then(async () => {
     console.log('数据库连接成功');
+    // 初始化管理员账号
+    const UserModel = require('../models/UserModel');
+    await UserModel.initAdminAccount();
   })
   .catch(err => {
     console.log('数据库连接失败=>', err);
